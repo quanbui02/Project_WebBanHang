2699c0409ff0d66f203e146adbc4b6cde5d9363f
Quan Bui <bhquaninit27@gmail.com>
2023-05-06 07:02:17 +0700

    upCreateActionControllerProduct

--- Action-Controller/ProductController/CreateProduct_action.php
+++ Action-Controller/ProductController/CreateProduct_action.php
@@ -3,13 +3,17 @@ include_once "C:/xampp/htdocs/Project_WebBanHang/Class-Model/Product.php";
 include_once "C:/xampp/htdocs/Project_WebBanHang/DAO/ProductDAO.php";
 session_start();
 $CategoryID =$_POST["GroupProduct_ID"];
-$ProductName = $_REQUEST["ProductName"];
-$ProductPrice = $_REQUEST["ProductPrice"];
-$ProductQuantity = $_REQUEST["ProductQuantity"];
-$ProductSize = $_REQUEST["ProductSize"];
-$ProductColor = $_REQUEST["ProductColor"];
-$ProductDescription = $_REQUEST["ProductDescription"];
+$ProductName = $_POST["ProductName"];
+$ProductPrice = $_POST["ProductPrice"];
+$ProductQuantity = $_POST["ProductQuantity"];
+$ProductSize = $_POST["ProductSize"];
+$ProductColor = $_POST["ProductColor"];
+$ProductDescription = $_POST["ProductDescription"];
 $Active = 1;
+$fileNameImg = $_FILES['ProductImage']['name'];
+$file = $_FILES['ProductImage']['tmp_name'];
+$path = "C:/xampp/htdocs/Project_WebBanHang/Upload/img".$fileNameImg;
+if(move_uploaded_file($file,$path)) {
     $newProduct = new Product(
         0,
         $CategoryID,
@@ -19,8 +23,14 @@ $Active = 1;
         $ProductSize,
         $ProductColor,
         $ProductDescription,
-        $Active
+        $Active,
+        $fileNameImg
     );
     createProduct($newProduct);
     header("Location: /Project_WebBanHang/Template-Views/Admin/Product/Index.php");
+}
+else {
+    echo "Tải tập tin thất bại";
+    header("Location: /Project_WebBanHang/Template-Views/Admin/Product/CreateProduce.php");
+}
 ?>
