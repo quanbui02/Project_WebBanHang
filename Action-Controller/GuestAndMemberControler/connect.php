63a6a43e8e06dea282e348ddca979fd62a28fccc
duyanh1762 <deadpool692.0@gmail.com>
2023-05-08 21:21:47 +0700

    Quan la gay

---
+++ Action-Controller/GuestAndMemberControler/connect.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "localhost";
+$username = "bexuanmailonto";
+$password = "170602cf";
+$database = "csdldoan";
+$conn = new mysqli("localhost", "bexuanmailonto", "170602cf","csdldoan");
+?>
