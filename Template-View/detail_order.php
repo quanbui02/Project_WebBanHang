55139a80b193108b7b5942575b46eb98e54218e9
duyanh1762 <deadpool692.0@gmail.com>
2023-05-13 22:24:22 +0700

    them css

--- Template-View/detail_order.php
+++ Template-View/detail_order.php
@@ -9,30 +9,67 @@ $listDetails = getOrderDetails(intval($orderID));
  ?>
  <html>
     <head>
-        <link rel="stylesheet" href=".">
+        <link rel="stylesheet" href="detail_order.css">
     </head>
     <body>
         <div class="header">
-            <p><?php echo "Thông tin đơn hàng ".$code; ?></p>
+            <div class="back"><button><a href="/DoAnCNW/Template-View/trangchu.php?search="> <-Trở về</a></button></div>
+            <div class="tag"><h2><?php echo "Thông tin đơn hàng ".$code; ?></h2></div>
         </div>
         <div class="infor">
-            <div class="date">Thời gian: <?php echo $date; ?></div>
-            <div class="status">Trạng thái: <?php echo $status ?></div>
+            <div class="date">
+                <div class="label">Thời gian:</div>
+                <div class="content"><?php echo $date; ?></div>   
+            </div>
+            <div class="status">
+                <div class="label">Trạng thái:</div>
+                <div class="content">
+                    <?php 
+                        if($status == "payed"){
+                        echo " Đang giao";
+                        }else{
+                        if($status == "completed"){
+                        echo " Đã giao"; 
+                        }else{
+                        echo " Chưa thanh toán";
+                        }
+                      } 
+                    ?>
+                </div>
+            </div>
             <div class="details">
+                <p>Danh sách sản phẩm</p>
                 <?php 
                    for($i = 0;$i<count($listDetails);$i++){
                     ?>
                        <div class="item">
-                          <div class="name"> Tên sản phẩm:  <?php echo $listDetails[$i]->getNameProduct(); ?></div>
-                          <div class="number"> Số lượng:  <?php echo $listDetails[$i]->getNumber(); ?></div>
-                          <div class="money"> Thành tiền: <?php echo $listDetails[$i]->getMoney(); ?> VND</div>
+                          <div class="name">
+                            <div class="label">Tên sản phẩm: </div>
+                            <div class="content"> <?php echo $listDetails[$i]->getNameProduct(); ?></div>
+                          </div>
+                          <div class="number-money"> 
+                            <div class="number">
+                                <div class="label" style="font-style:italic;font-weight:14px">->Số lượng:</div>
+                                <div class="content" >x<?php echo $listDetails[$i]->getNumber(); ?></div>
+                            </div>
+                            <div class="money"> 
+                                 <div class="label" style="font-style:italic;font-weight:14px">Thành tiền:</div>
+                                 <div class="content" ><?php echo $listDetails[$i]->getMoney(); ?> VND</div>
+                            </div> 
+                          </div>
                        </div>
                     <?php
                    }
                 ?>
             </div>
-            <div class="sum-money">Tổng tiền: <?php echo getOrderByID($orderID)->sumMoney(); ?> VND</div>
-            <div class="purchase">Số tiền thanh toán(Sau khi sử dụng mã giảm giá nếu có): <?php echo getOrderByID($orderID)->sumMoneyWithGift(); ?> VND </div>
+            <div class="sum-money">
+                <div class="content"><?php echo getOrderByID($orderID)->sumMoney(); ?> VND</div>
+                <div class="label" style="font-style:italic;font-weight:14px">Tổng tiền: </div>
+            </div>
+            <div class="purchase">
+                <div class="content"><?php echo getOrderByID($orderID)->sumMoneyWithGift(); ?> VND</div> 
+                <div class="label" style="font-style:italic;font-weight:14px">Số tiền thanh toán(Sau khi sử dụng mã giảm giá nếu có):</div>
+            </div>
         </div>
     </body>
  </html>
