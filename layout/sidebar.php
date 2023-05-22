c3758dbc6fdbd324ea0aa97ae1480ad3c3d8b256
Quan Bui <bhquaninit27@gmail.com>
2023-05-22 14:18:58 +0700

    usingAjaxLoadPage

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -1,10 +1,12 @@
 <?php
 class ItemSidebar
-{
+{   
+    public $id;
     public $name;
     public $href;
-    public function __construct($name, $href)
-    {
+    public function __construct($id,$name, $href)
+    {   
+        $this->id = $id;
         $this->name = $name;
         $this->href = $href;
     }
@@ -14,19 +16,19 @@ function sidebar()
 {
 
     $myArray = [
-        new ItemSidebar("Trang chủ", "/Project_WebBanHang/Template-Views/Admin/Index.php"),
-        new ItemSidebar("Sản phẩm", "/Project_WebBanHang/Template-Views/Admin/Product/Index.php"),
-        new ItemSidebar("Danh mục sản phẩm", "/Project_WebBanHang/Template-Views/Admin/Category/Index.php"),
-        new ItemSidebar("Khách hàng", "/Project_WebBanHang/Template-Views/Admin/User/Index.php"),
-        new ItemSidebar("Đơn hàng", "javascript:void(0);"),
-        new ItemSidebar("Khuyến mãi", "/Project_WebBanHang/Template-Views/Admin/GiftCode/Index.php"),
-        new ItemSidebar("Đăng xuất", "/Project_WebBanHang/Action-Controller/LoginController/Logout_action.php")
+        new ItemSidebar("Page1","Trang chủ", "/Project_WebBanHang/Template-Views/Admin/Index.php"),
+        new ItemSidebar("Page2","Sản phẩm", "/Project_WebBanHang/Template-Views/Admin/Product/Index.php"),
+        new ItemSidebar("Page3","Danh mục sản phẩm", "/Project_WebBanHang/Template-Views/Admin/Category/Index.php"),
+        new ItemSidebar("Page4","Khách hàng", "/Project_WebBanHang/Template-Views/Admin/User/Index.php"),
+        new ItemSidebar("Page5","Đơn hàng", "javascript:void(0);"),
+        new ItemSidebar("Page6","Khuyến mãi", "/Project_WebBanHang/Template-Views/Admin/GiftCode/Index.php"),
+        new ItemSidebar("Page7","Đăng xuất", "/Project_WebBanHang/Action-Controller/LoginController/Logout_action.php")
     ];
 
     $html = '';
     foreach ($myArray as $item) {
         $html .= '<li class="nav-item dropdown"> 
-                <a href="' . $item->href . '">
+                <a href="' . $item->href . '" class="nav-link"  data-page="'.$item->href.'">
                     <span class="title">' . $item->name . '</span>
                 </a>
             </li>
@@ -35,3 +37,4 @@ function sidebar()
 
     return $html;
 }
+// href="' . $item->href . '"
