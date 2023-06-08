328bc3ba10223fb19cde0f8c7a87804560610f20
AnhPham116 <anhph11.work@gmail.com>
2023-06-08 20:14:19 +0700

    fixcss_listcatedete

--- Template-Views/Admin/Category/listCategoryDeleted.php
+++ Template-Views/Admin/Category/listCategoryDeleted.php
@@ -11,7 +11,7 @@ $lengtGroup = count($listGroup);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/Project_WebBanHang/assets/css/category_de.css">
+    <link rel="stylesheet" href="/Project_WebBanHang/assets/css/cate_de.css">
     <title>Danh mục đã xoá</title>
 </head>
 <body>
