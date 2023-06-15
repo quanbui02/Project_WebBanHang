5a279e45aba090daffa112d98afa98c47086feb2
luongbodoi <88548776+luongbodoi@users.noreply.github.com>
2023-06-15 21:39:10 +0700

    login ui

--- ultis/state.php
+++ ultis/state.php
@@ -2,15 +2,15 @@
 function statusOrder($statusOrder)
 {
     if ($statusOrder == "cart") {
-        return '<div style="padding: 4px 12px; background-color: #ea815e; color: white; border-radius: 8px;">Trong giỏ hàng </div>';
+        return '<div style="padding: 4px 12px; width: fit-content; background-color: #ea815e; color: white; border-radius: 8px;">Trong giỏ hàng </div>';
     } else if ($statusOrder == "payed") {
-        return '<div style="padding: 4px 12px; background-color: #F7375D; color: white; border-radius: 8px;">Đã thanh toán </div>';
+        return '<div style="padding: 4px 12px; width: fit-content; background-color: #F7375D; color: white; border-radius: 8px;">Đã thanh toán </div>';
     } else if ($statusOrder == "confirm") {
-        return '<div style="padding: 4px 12px; background-color: #D448FF; color: white; border-radius: 8px;">Đã xác nhận </div>';
+        return '<div style="padding: 4px 12px; width: fit-content; background-color: #D448FF; color: white; border-radius: 8px;">Đã xác nhận </div>';
     } else if ($statusOrder == "completed") {
-        return '<div style="padding: 4px 12px; background-color: #248CF3; color: white; border-radius: 8px;">Hoàn thành đơn đặt hàng</div>';
+        return '<div style="padding: 4px 12px; width: fit-content; background-color: #248CF3; color: white; border-radius: 8px;">Hoàn thành đơn đặt hàng</div>';
     } else {
-        return '<div style="padding: 4px 12px; background-color: red; color: white; border-radius: 8px;>Lỗi</div>';
+        return '<div style="padding: 4px 12px; width: fit-content; background-color: red; color: white; border-radius: 8px;>Lỗi</div>';
     }
 }
 ?>
