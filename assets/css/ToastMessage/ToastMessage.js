88753791bb6bd41050225c3ba67ada4fe18d0a23
luongbodoi <88548776+luongbodoi@users.noreply.github.com>
2023-06-20 00:12:44 +0700

    link header menu

--- assets/css/ToastMessage/ToastMessage.js
+++ assets/css/ToastMessage/ToastMessage.js
@@ -30,17 +30,16 @@ function toast({ title = "", message = "", type = "info", duration = 3000 }) {
     toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
 
     toast.innerHTML = `
-                      <div class="toast__icon">
-                          <i class="${icon}"></i>
-                      </div>
-                      <div class="toast__body">
-                          <h3 class="toast__title">${title}</h3>
-                          <p class="toast__msg">${message}</p>
-                      </div>
-                      <div class="toast__close">
-                          <i class="fas fa-times"></i>
-                      </div>
-                  `;
+      <div class="toast__icon">
+        <i class="${icon}"></i>
+          </div>
+        <div class="toast__body">
+          <h3 class="toast__title">${title}</h3>
+          <p class="toast__msg">${message}</p>
+        </div>
+        <div class="toast__close">
+           <i class="fas fa-times"></i>
+      </div>`;
     main.appendChild(toast);
   }
 }
