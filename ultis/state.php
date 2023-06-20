7d193183326d8e026280b6db3b3ba53d048252c5
Quan Bui <bhquaninit27@gmail.com>
2023-06-20 13:51:35 +0700

    updateUIgiftCode

--- ultis/state.php
+++ ultis/state.php
@@ -4,11 +4,11 @@ function statusOrder($statusOrder)
     if ($statusOrder == "cart") {
         return '<div style="padding: 4px 12px; width: fit-content; background-color: #ea815e; color: white; border-radius: 8px;">Trong giỏ hàng </div>';
     } else if ($statusOrder == "payed") {
-        return '<div style="padding: 4px 12px; width: fit-content; background-color: #F7375D; color: white; border-radius: 8px;">Đã thanh toán </div>';
+        return '<div style="padding: 4px 12px; width: fit-content; background-color: #F7375D; color: white; border-radius: 8px;">Đàng chờ xác nhận</div>';
     } else if ($statusOrder == "confirm") {
-        return '<div style="padding: 4px 12px; width: fit-content; background-color: #D448FF; color: white; border-radius: 8px;">Đã xác nhận </div>';
+        return '<div style="padding: 4px 12px; width: fit-content; background-color: #D448FF; color: white; border-radius: 8px;">Đang giao</div>';
     } else if ($statusOrder == "completed") {
-        return '<div style="padding: 4px 12px; width: fit-content; background-color: #248CF3; color: white; border-radius: 8px;">Hoàn thành đơn đặt hàng</div>';
+        return '<div style="padding: 4px 12px; width: fit-content; background-color: #248CF3; color: white; border-radius: 8px;">Hoàn thành</div>';
     } else {
         return '<div style="padding: 4px 12px; width: fit-content; background-color: red; color: white; border-radius: 8px;>Lỗi</div>';
     }
