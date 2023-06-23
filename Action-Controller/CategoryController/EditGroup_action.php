a3a057913cc20194a4e9cbfeb90eec528db46469
Quan Bui <bhquaninit27@gmail.com>
2023-06-24 01:56:55 +0700

    EditCategoryWithPopUp

--- Action-Controller/CategoryController/EditGroup_action.php
+++ Action-Controller/CategoryController/EditGroup_action.php
@@ -3,11 +3,12 @@ include_once "C:/xampp/htdocs/Project_WebBanHang/Class-Model/GroupProduct.php";
 include_once "C:/xampp/htdocs/Project_WebBanHang/DAO/GroupDAO.php";
 session_start();
 $GroupName = $_REQUEST["CategoryName"];
+$ID = intval($_GET["id"]);
 if(trim($GroupName) == ""){
     $_SESSION["err_value"] = "Dien du thong tin du lieu";
     header("Location: /Project_WebBanHang/Template-Views/Admin/Category/editCategory.php");
 }else{
-    $updateGroup = new GroupProduct((int)$_SESSION["getInfoGroup"]->getGrID(),$GroupName,(int)$_SESSION["getInfoGroup"]->getAct());
+    $updateGroup = new GroupProduct($ID,$GroupName,1);
     updateGroup($updateGroup);
     $_SESSION["err_value"] = "";
     header("Location: /Project_WebBanHang/Template-Views/Admin/Category/Index.php");   
