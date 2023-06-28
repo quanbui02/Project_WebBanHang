69304d0d4b399807f63b96cbf8fef0d201d50478
luongbodoi <88548776+luongbodoi@users.noreply.github.com>
2023-06-28 21:10:36 +0700

    home page ui

--- Template-Views/Admin/Shared/Index.php
+++ Template-Views/Admin/Shared/Index.php
@@ -1,6 +1,4 @@
 <?php
-
-include_once "C:/xampp/htdocs/Project_WebBanHang/layout/sidebar.php";
 include_once "C:/xampp/htdocs/Project_WebBanHang/Data/ConnectToDatabase.php";
 
 session_start();
@@ -31,147 +29,304 @@ try {
     $conn->close();
 }
 
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/Project_WebBanHang/assets/css/main.css">
-    <link rel="stylesheet" href="/Project_WebBanHang/assets/css/Home/index.css">
+    <link rel="stylesheet" href="../../../assets/css/mainHeader.css">
+    <link rel="stylesheet" href="../../../assets/css/popUpModel.css">
+    <link rel="stylesheet" href="../../../assets/css/pagination.css">
+    <link rel="stylesheet" href="../../../assets/css/ToastMessage/ToastMessage.css">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="Index.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../../assets/css/Home/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+
     <title>Trang chủ Admin</title>
 </head>
 
-<body>
-    <div class="container_flex">
-        <div class="side-nav">
-            <div class="side-nav-inner">
-                <?php
-                echo sidebar();
-                ?>
-            </div>
-        </div>
-        <div class="container_admin">
-            <div class="content_Admin">
-                TRANG CHỦ QUẢN TRỊ
-            </div>
-            <div class="content">
-                <ul class="list-orders">
-                    <li class="item-order" style="background-color: #ea815e;">
-                        <p class="title">Đơn hàng đang chờ xác nhận</p>
-                        <p class="quantity">
-                            <?php
-                            if (isset($_SESSION['ChoXacNhan'])) {
-                                echo $_SESSION['ChoXacNhan'];
-                            } else {
-                                echo $soDonDangChoXacNhan;
-                            }
-                            ?>
-                        </p>
-                    </li>
-                    <li class="item-order" style="background-color:#F7375D;">
-                        <p class="title">Đơn hàng đang giao</p>
-                        <p class="quantity">
-                            <?php
-                            if (isset($_SESSION['DangGiao'])) {
-                                echo $_SESSION['DangGiao'];
-                            } else {
-                                echo $soDonDangGiao;
-                            }
-                            ?>
-                        </p>
-                    </li>
-                    <li class="item-order" style="background-color:#D448FF;">
-                        <p class="title">Đơn hàng đã giao</p>
-                        <p class="quantity">
-                            <?php
-                            if (isset($_SESSION['DaGiao'])) {
-                                echo $_SESSION['DaGiao'];
-                            } else {
-                                echo $soDonDaGiao;
-                            }
-                            ?>
-                        </p>
-                    </li>
-                    <li class="item-order" style="background-color:red;">
-                        <p class="title">Đơn hàng đã huỷ</p>
-                        <p class="quantity">
-                            <?php
-                            if (isset($_SESSION['DaHuy'])) {
-                                echo $_SESSION['DaHuy'];
-                            } else {
-                                echo $soDonDaHuy;
-                            }
-                            ?>
-                        </p>
-                    </li>
-                </ul>
-                <?php
-                unset($_SESSION['ChoXacNhan'], $_SESSION['DangGiao'], $_SESSION['DaGiao'], $_SESSION['DaHuy']);
-                ?>
-                <div class="form-date">
-                    <div>
-                        <form action="/Project_WebBanHang/Action-Controller/ThongKeController/ThongKe_action.php" method="post">
-                            <label class="title-form" for="DOANHTHU">DOANH THU:</label>
-                            <p>Từ:</p>
-                            <input type="datetime-local" id="DOANHTHU1" name="DOANHTHU1" value="<?php echo isset($_SESSION['Ngaydau']) ? $_SESSION['Ngaydau'] : ''; ?>">
-                            <p>Đến:</p>
-                            <input type="datetime-local" id="DOANHTHU2" name="DOANHTHU2" value="<?php echo isset($_SESSION['NgaySau']) ? $_SESSION['NgaySau'] : ''; ?>">
-                            <br>
-                            <br>
-                            <button style="border:1px solid #ccc;border-radius:3px;" type="submit">Thống kê</button>
-                        </form>
-                    </div>
-                    <?php unset($_SESSION['NgaySau'], $_SESSION['Ngaydau']) ?>
-                    <?php
-                    if (isset($_SESSION['tongTien'])) {
-                        $tongTien = $_SESSION['tongTien'];
-                        echo "<p>Tổng tiền: " . $tongTien . "</p>";
-                        unset($_SESSION['tongTien']);
-                    } else {
-                        echo "0";
-                    }
-                    ?>
-                    <?php $thang = $_SESSION['listThangThongKe']; ?>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Tháng</th>
-                                <th>Năm</th>
-                                <th>Tổng tiền</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($thang as $item) : ?>
-                                <tr>
-                                    <td><?php echo $item['thang']; ?></td>
-                                    <td><?php echo $item['nam']; ?></td>
-                                    <td><?php echo $item['tongTien']; ?></td>
-                                </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                </div>
-                <?php
-                if (isset($_SESSION['error-date'])) {
-                    $errorDate = $_SESSION['error-date'];
-                    echo "<p> " . $errorDate . "</p>";
-                    unset($_SESSION['error-date']);
-                }
-                ?>
-            </div>
-        </div>
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
 
+<body>
+    <div class="job">
+        <div class="header">
+            <div class="logo">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path xmlns="http://www.w3.org/2000/svg" d="M512 503.5H381.7a48 48 0 01-45.3-32.1L265 268.1l-9-25.5 2.7-124.6L338.2 8.5l23.5 67.1L512 503.5z" fill="#0473ff" data-original="#28b446" />
+                    <path xmlns="http://www.w3.org/2000/svg" fill="#0473ff" data-original="#219b38" d="M361.7 75.6L265 268.1l-9-25.5 2.7-124.6L338.2 8.5z" />
+                    <path xmlns="http://www.w3.org/2000/svg" d="M338.2 8.5l-82.2 234-80.4 228.9a48 48 0 01-45.3 32.1H0l173.8-495h164.4z" fill="#0473ff" data-original="#518ef8" />
+                </svg>
+                Admin
+            </div>
+            <div class="header-menu">
+                <a href="/Project_WebBanHang/Template-Views/Admin/Shared/Index.php" class="active">Trang chủ</a>
+                <a href="/Project_WebBanHang/Template-Views/Admin/Product/Index.php">Sản phẩm</a>
+                <a href="/Project_WebBanHang/Template-Views/Admin/Category/Index.php">Danh mục sản phẩm</a>
+                <a href="/Project_WebBanHang/Template-Views/Admin/User/Index.php">Khách hàng</a>
+                <a href="/Project_WebBanHang/Template-Views/Admin/Order/Index.php">Đơn hàng </a>
+                <a href="/Project_WebBanHang/Template-Views/Admin/GiftCode/Index.php">Khuyễn Mãi</a>
+            </div>
+            <div class="user-settings">
+                <a href="/Project_WebBanHang/Action-Controller/LoginController/Logout_action.php" class="logout-btn">Đăng xuất</a>
+            </div>
+        </div>
     </div>
+
+    <main>
+        <div id="chart_div"></div>
+        <h2>Beautiful Google Charts</h2>
+        <h5>Daily Page Hits</h5>
+        <div id="bar-chart"></div>
+        <h5>Traffic Over Time</h5>
+        <div id="line-chart"></div>
+        <h5>Page Hits per Country</h5>
+        <div id="pie-chart"></div>
+        <p>Google Charts can be <a href="https://google-developers.appspot.com/chart/">found here</a>.</p>
+    </main>
 </body>
+
+<script> 
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawCharts);
+function drawCharts() {
+  
+ //Char User
+  var data = new google.visualization.DataTable();
+      data.addColumn('number', 'X');
+      data.addColumn('number', 'Dogs');
+
+      data.addRows([
+        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+        [66, 70], [67, 72], [68, 75], [69, 80]
+      ]);
+
+      var options = {
+        hAxis: {
+          title: 'Time'
+        },
+        vAxis: {
+          title: 'Popularity'
+        },
+        backgroundColor: '#f1f8e9'
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+  // BEGIN BAR CHART
+  /*
+  // create zero data so the bars will 'grow'
+  var barZeroData = google.visualization.arrayToDataTable([
+    ['Day', 'Page Views', 'Unique Views'],
+    ['Sun',  0,      0],
+    ['Mon',  0,      0],
+    ['Tue',  0,      0],
+    ['Wed',  0,      0],
+    ['Thu',  0,      0],
+    ['Fri',  0,      0],
+    ['Sat',  0,      0]
+  ]);
+	*/
+  // actual bar chart data
+  var barData = google.visualization.arrayToDataTable([
+    ['Day', 'Page Views', 'Unique Views'],
+    ['Sun',  1050,      600],
+    ['Mon',  1370,      910],
+    ['Tue',  660,       400],
+    ['Wed',  1030,      540],
+    ['Thu',  1000,      480],
+    ['Fri',  1170,      960],
+    ['Sat',  660,       320]
+  ]);
+  // set bar chart options
+  var barOptions = {
+    focusTarget: 'category',
+    backgroundColor: 'transparent',
+    colors: ['cornflowerblue', 'tomato'],
+    fontName: 'Open Sans',
+    chartArea: {
+      left: 50,
+      top: 10,
+      width: '100%',
+      height: '70%'
+    },
+    bar: {
+      groupWidth: '80%'
+    },
+    hAxis: {
+      textStyle: {
+        fontSize: 11
+      }
+    },
+    vAxis: {
+      minValue: 0,
+      maxValue: 1500,
+      baselineColor: '#DDD',
+      gridlines: {
+        color: '#DDD',
+        count: 4
+      },
+      textStyle: {
+        fontSize: 11
+      }
+    },
+    legend: {
+      position: 'bottom',
+      textStyle: {
+        fontSize: 12
+      }
+    },
+    animation: {
+      duration: 1200,
+      easing: 'out',
+			startup: true
+    }
+  };
+  // draw bar chart twice so it animates
+  var barChart = new google.visualization.ColumnChart(document.getElementById('bar-chart'));
+  //barChart.draw(barZeroData, barOptions);
+  barChart.draw(barData, barOptions);
+  
+  // BEGIN LINE GRAPH
+  
+  function randomNumber(base, step) {
+    return Math.floor((Math.random()*step)+base);
+  }
+  function createData(year, start1, start2, step, offset) {
+    var ar = [];
+    for (var i = 0; i < 12; i++) {
+      ar.push([new Date(year, i), randomNumber(start1, step)+offset, randomNumber(start2, step)+offset]);
+    }
+    return ar;
+  }
+  var randomLineData = [
+    ['Year', 'Page Views', 'Unique Views']
+  ];
+  for (var x = 0; x < 7; x++) {
+    var newYear = createData(2010+x, 10000, 5000, 4000, 800*Math.pow(x,2));
+    for (var n = 0; n < 12; n++) {
+      randomLineData.push(newYear.shift());
+    }
+  }
+  var lineData = google.visualization.arrayToDataTable(randomLineData);
+  
+	
+  var animLineData = [
+    ['Year', 'Page Views', 'Unique Views']
+  ];
+  for (var x = 0; x < 7; x++) {
+    var zeroYear = createData(2007+x, 0, 0, 0, 0);
+    for (var n = 0; n < 12; n++) {
+      animLineData.push(zeroYear.shift());
+    }
+  }
+  var zeroLineData = google.visualization.arrayToDataTable(animLineData);
+	
+
+  var lineOptions = {
+    backgroundColor: 'transparent',
+    colors: ['cornflowerblue', 'tomato'],
+    fontName: 'Open Sans',
+    focusTarget: 'category',
+    chartArea: {
+      left: 50,
+      top: 10,
+      width: '100%',
+      height: '70%'
+    },
+    hAxis: {
+      //showTextEvery: 12,
+      textStyle: {
+        fontSize: 11
+      },
+      baselineColor: 'transparent',
+      gridlines: {
+        color: 'transparent'
+      }
+    },
+    vAxis: {
+      minValue: 0,
+      maxValue: 50000,
+      baselineColor: '#DDD',
+      gridlines: {
+        color: '#DDD',
+        count: 4
+      },
+      textStyle: {
+        fontSize: 11
+      }
+    },
+    legend: {
+      position: 'bottom',
+      textStyle: {
+        fontSize: 12
+      }
+    },
+    animation: {
+      duration: 1200,
+      easing: 'out',
+			startup: true
+    }
+  };
+
+  var lineChart = new google.visualization.LineChart(document.getElementById('line-chart'));
+  //lineChart.draw(zeroLineData, lineOptions);
+  lineChart.draw(lineData, lineOptions);
+  
+  // BEGIN PIE CHART
+  
+  // pie chart data
+  var pieData = google.visualization.arrayToDataTable([
+    ['Country', 'Page Hits'],
+    ['USA',      7242],
+    ['Canada',   4563],
+    ['Mexico',   1345],
+    ['Sweden',    946],
+    ['Germany',  2150]
+  ]);
+  // pie chart options
+  var pieOptions = {
+    backgroundColor: 'transparent',
+    pieHole: 0.4,
+    colors: [ "cornflowerblue", 
+              "olivedrab", 
+              "orange", 
+              "tomato", 
+              "crimson", 
+              "purple", 
+              "turquoise", 
+              "forestgreen", 
+              "navy", 
+              "gray"],
+    pieSliceText: 'value',
+    tooltip: {
+      text: 'percentage'
+    },
+    fontName: 'Open Sans',
+    chartArea: {
+      width: '100%',
+      height: '94%'
+    },
+    legend: {
+      textStyle: {
+        fontSize: 13
+      }
+    }
+  };
+  // draw pie chart
+  var pieChart = new google.visualization.PieChart(document.getElementById('pie-chart'));
+  pieChart.draw(pieData, pieOptions);
+}
+</script>
 
 </html>
