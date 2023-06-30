1ef456cd19e4ab041b3e5be8c06797b30d4fe03d
Pham Hung Luong <88548776+PhamHungLuong@users.noreply.github.com>
2023-06-30 17:09:37 +0700

    home page ui (#7)
    * home page ui
    # home page ui
    
    ---------
    
    Co-authored-by: luongbodoi <88548776+luongbodoi@users.noreply.github.com>

--- Template-Views/Admin/Shared/Index.php
+++ Template-Views/Admin/Shared/Index.php
@@ -1,177 +1,255 @@
 <?php
-
-include_once "C:/xampp/htdocs/Project_WebBanHang/layout/sidebar.php";
 include_once "C:/xampp/htdocs/Project_WebBanHang/Data/ConnectToDatabase.php";
 
 session_start();
 if ($_SESSION["login"] == false) {
-    header("Location: /Project_WebBanHang/Template-Views/Admin/LoginAdmin/Index.php");
+  header("Location: /Project_WebBanHang/Template-Views/Admin/LoginAdmin/Index.php");
 }
 $conn = connectDb();
 try {
-    $query = "SELECT 
+  $query = "SELECT 
         COUNT(CASE WHEN status = 'payed' THEN 1 END) as soDonDangChoXacNhan,
         COUNT(CASE WHEN status = 'confirm' THEN 1 END) as soDonDangGiao,
         COUNT(CASE WHEN status = 'completed' THEN 1 END) as soDonDaGiao,
         COUNT(CASE WHEN status = 'destroyed' THEN 1 END) as soDonDaHuy
         FROM `order`";
-    $result = $conn->query($query);
-    if ($result && $result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        $soDonDangChoXacNhan = $row['soDonDangChoXacNhan'];
-        $soDonDangGiao = $row['soDonDangGiao'];
-        $soDonDaGiao = $row['soDonDaGiao'];
-        $soDonDaHuy = $row['soDonDaHuy'];
-    } else {
-        throw new Exception("Không có dữ liệu");
-    }
+  $result = $conn->query($query);
+  if ($result && $result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $soDonDangChoXacNhan = $row['soDonDangChoXacNhan'];
+    $soDonDangGiao = $row['soDonDangGiao'];
+    $soDonDaGiao = $row['soDonDaGiao'];
+    $soDonDaHuy = $row['soDonDaHuy'];
+  } else {
+    throw new Exception("Không có dữ liệu");
+  }
 } catch (Exception $e) {
-    $_SESSION['error-date'] = $e->getMessage();
+  $_SESSION['error-date'] = $e->getMessage();
 } finally {
-    $conn->close();
+  $conn->close();
 }
-
-
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/Project_WebBanHang/assets/css/main.css">
-    <link rel="stylesheet" href="/Project_WebBanHang/assets/css/Home/index.css">
+    <link rel="stylesheet" href="../../../assets/css/mainHeader.css">
+    <link rel="stylesheet" href="../../../assets/css/popUpModel.css">
+    <link rel="stylesheet" href="../../../assets/css/pagination.css">
+    <link rel="stylesheet" href="../../../assets/css/ToastMessage/ToastMessage.css">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="Index.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../../assets/css/Home/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+        crossorigin="anonymous" />
+
     <title>Trang chủ Admin</title>
 </head>
 
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
 <body>
-    <div class="container_flex">
-        <div class="side-nav">
-            <div class="side-nav-inner">
-                <?php
-                echo sidebar();
-                ?>
-            </div>
-        </div>
-        <div class="container_admin">
-            <div class="content_Admin">
-                TRANG CHỦ QUẢN TRỊ
-            </div>
-            <div class="content">
-                <ul class="list-orders">
-                    <li class="item-order" style="background-color: #ea815e;">
-                        <p class="title">Đơn hàng đang chờ xác nhận</p>
-                        <p class="quantity">
-                            <?php
-                            if (isset($_SESSION['ChoXacNhan'])) {
-                                echo $_SESSION['ChoXacNhan'];
-                            } else {
-                                echo $soDonDangChoXacNhan;
-                            }
-                            ?>
-                        </p>
-                    </li>
-                    <li class="item-order" style="background-color:#F7375D;">
-                        <p class="title">Đơn hàng đang giao</p>
-                        <p class="quantity">
-                            <?php
-                            if (isset($_SESSION['DangGiao'])) {
-                                echo $_SESSION['DangGiao'];
-                            } else {
-                                echo $soDonDangGiao;
-                            }
-                            ?>
-                        </p>
-                    </li>
-                    <li class="item-order" style="background-color:#D448FF;">
-                        <p class="title">Đơn hàng đã giao</p>
-                        <p class="quantity">
-                            <?php
-                            if (isset($_SESSION['DaGiao'])) {
-                                echo $_SESSION['DaGiao'];
-                            } else {
-                                echo $soDonDaGiao;
-                            }
-                            ?>
-                        </p>
-                    </li>
-                    <li class="item-order" style="background-color:red;">
-                        <p class="title">Đơn hàng đã huỷ</p>
-                        <p class="quantity">
-                            <?php
-                            if (isset($_SESSION['DaHuy'])) {
-                                echo $_SESSION['DaHuy'];
-                            } else {
-                                echo $soDonDaHuy;
-                            }
-                            ?>
-                        </p>
-                    </li>
-                </ul>
-                <?php
-                unset($_SESSION['ChoXacNhan'], $_SESSION['DangGiao'], $_SESSION['DaGiao'], $_SESSION['DaHuy']);
-                ?>
-                <div class="form-date">
-                    <div>
-                        <form action="/Project_WebBanHang/Action-Controller/ThongKeController/ThongKe_action.php" method="post">
-                            <label class="title-form" for="DOANHTHU">DOANH THU:</label>
-                            <p>Từ:</p>
-                            <input type="datetime-local" id="DOANHTHU1" name="DOANHTHU1" value="<?php echo isset($_SESSION['Ngaydau']) ? $_SESSION['Ngaydau'] : ''; ?>">
-                            <p>Đến:</p>
-                            <input type="datetime-local" id="DOANHTHU2" name="DOANHTHU2" value="<?php echo isset($_SESSION['NgaySau']) ? $_SESSION['NgaySau'] : ''; ?>">
-                            <br>
-                            <br>
-                            <button style="border:1px solid #ccc;border-radius:3px;" type="submit">Thống kê</button>
-                        </form>
-                    </div>
-                    <?php unset($_SESSION['NgaySau'], $_SESSION['Ngaydau']) ?>
-                    <?php
-                    if (isset($_SESSION['tongTien'])) {
-                        $tongTien = $_SESSION['tongTien'];
-                        echo "<p>Tổng tiền: " . $tongTien . "</p>";
-                        unset($_SESSION['tongTien']);
-                    } else {
-                        echo "0";
-                    }
-                    ?>
-                    <?php $thang = $_SESSION['listThangThongKe']; ?>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Tháng</th>
-                                <th>Năm</th>
-                                <th>Tổng tiền</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($thang as $item) : ?>
-                                <tr>
-                                    <td><?php echo $item['thang']; ?></td>
-                                    <td><?php echo $item['nam']; ?></td>
-                                    <td><?php echo $item['tongTien']; ?></td>
-                                </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
+    <div class="content">
+        <div class="job">
+            <div class="header">
+                <div class="logo">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path xmlns="http://www.w3.org/2000/svg"
+                            d="M512 503.5H381.7a48 48 0 01-45.3-32.1L265 268.1l-9-25.5 2.7-124.6L338.2 8.5l23.5 67.1L512 503.5z"
+                            fill="#0473ff" data-original="#28b446" />
+                        <path xmlns="http://www.w3.org/2000/svg" fill="#0473ff" data-original="#219b38"
+                            d="M361.7 75.6L265 268.1l-9-25.5 2.7-124.6L338.2 8.5z" />
+                        <path xmlns="http://www.w3.org/2000/svg"
+                            d="M338.2 8.5l-82.2 234-80.4 228.9a48 48 0 01-45.3 32.1H0l173.8-495h164.4z" fill="#0473ff"
+                            data-original="#518ef8" />
+                    </svg>
+                    Admin
                 </div>
-                <?php
-                if (isset($_SESSION['error-date'])) {
-                    $errorDate = $_SESSION['error-date'];
-                    echo "<p> " . $errorDate . "</p>";
-                    unset($_SESSION['error-date']);
-                }
-                ?>
+                <div class="header-menu">
+                    <a href="/Project_WebBanHang/Template-Views/Admin/Shared/Index.php" class="active">Trang chủ</a>
+                    <a href="/Project_WebBanHang/Template-Views/Admin/Product/Index.php">Sản phẩm</a>
+                    <a href="/Project_WebBanHang/Template-Views/Admin/Category/Index.php">Danh mục sản phẩm</a>
+                    <a href="/Project_WebBanHang/Template-Views/Admin/User/Index.php">Khách hàng</a>
+                    <a href="/Project_WebBanHang/Template-Views/Admin/Order/Index.php">Đơn hàng </a>
+                    <a href="/Project_WebBanHang/Template-Views/Admin/GiftCode/Index.php">Khuyễn Mãi</a>
+                </div>
+                <div class="user-settings">
+                    <a href="/Project_WebBanHang/Action-Controller/LoginController/Logout_action.php"
+                        class="logout-btn">Đăng xuất</a>
+                </div>
             </div>
         </div>
 
+        <div class="row main-content">
+            <div class="col-xl-3 col-sm-6 mb-3">
+                <div class="card text-white bg-primary o-hidden h-100">
+                    <div class="card-body">
+                        <div class="card-body-icon">
+                            <i class="fa fa-fw fa-comments"></i>
+                        </div>
+                        <div class="mr-5"> <?php
+                                if (isset($_SESSION['ChoXacNhan'])) {
+                                  echo $_SESSION['ChoXacNhan'];
+                                } else {
+                                  echo $soDonDangChoXacNhan;
+                                }
+                                ?> Đơn hàng chờ xác nhận</div>
+                    </div>
+                    <a class="card-footer text-white clearfix small z-1" href="#">
+                        <span class="float-left">View Details</span>
+                        <span class="float-right">
+                            <i class="fa fa-angle-right"></i>
+                        </span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 mb-3">
+                <div class="card text-white bg-warning o-hidden h-100">
+                    <div class="card-body">
+                        <div class="card-body-icon">
+                            <i class="fa fa-fw fa-list"></i>
+                        </div>
+                        <div class="mr-5"><?php
+                              if (isset($_SESSION['DangGiao'])) {
+                                echo $_SESSION['DangGiao'];
+                              } else {
+                                echo $soDonDangGiao;
+                              }
+                              ?> Đơn hàng đang giao</div>
+                    </div>
+                    <a class="card-footer text-white clearfix small z-1" href="#">
+                        <span class="float-left">View Details</span>
+                        <span class="float-right">
+                            <i class="fa fa-angle-right"></i>
+                        </span>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-sm-6 mb-3">
+                <div class="card text-white bg-success o-hidden h-100">
+                    <div class="card-body">
+                        <div class="card-body-icon">
+                            <i class="fa fa-fw fa-shopping-cart"></i>
+                        </div>
+                        <div class="mr-5"><?php
+                              if (isset($_SESSION['DaGiao'])) {
+                                echo $_SESSION['DaGiao'];
+                              } else {
+                                echo $soDonDaGiao;
+                              }
+                              ?> Đơn hàng đã giao</div>
+                    </div>
+                    <a class="card-footer text-white clearfix small z-1" href="#">
+                        <span class="float-left">View Details</span>
+                        <span class="float-right">
+                            <i class="fa fa-angle-right"></i>
+                        </span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 mb-3">
+                <div class="card text-white bg-danger o-hidden h-100">
+                    <div class="card-body">
+                        <div class="card-body-icon">
+                            <i class="fa fa-fw fa-support"></i>
+                        </div>
+                        <div class="mr-5"> <?php
+                                if (isset($_SESSION['DaHuy'])) {
+                                  echo $_SESSION['DaHuy'];
+                                } else {
+                                  echo $soDonDaHuy;
+                                }
+                                ?> Đơn hàng đã hủy</div>
+                    </div>
+                    <a class="card-footer text-white clearfix small z-1" href="#">
+                        <span class="float-left">View Details</span>
+                        <span class="float-right">
+                            <i class="fa fa-angle-right"></i>
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <?php
+          unset($_SESSION['ChoXacNhan'], $_SESSION['DangGiao'], $_SESSION['DaGiao'], $_SESSION['DaHuy']);
+         ?>
+        <div class="chart">
+            <h2 class="title-form" for="DOANHTHU">DOANH THU:</h2>
+            <form action="/Project_WebBanHang/Action-Controller/ThongKeController/ThongKe_action.php" method="post"
+                class="form-container">
+                <div class="input-group">
+                    <p>Từ:</p>
+                    <input type="datetime-local" id="DOANHTHU1" name="DOANHTHU1"
+                        value="<?php echo isset($_SESSION['Ngaydau']) ? $_SESSION['Ngaydau'] : ''; ?>">
+                </div>
+                <div class="input-group">
+                    <p>Đến:</p>
+                    <input type="datetime-local" id="DOANHTHU2" name="DOANHTHU2"
+                        value="<?php echo isset($_SESSION['NgaySau']) ? $_SESSION['NgaySau'] : ''; ?>">
+                </div>
+                <button type="submit" class="btn-submit">Thống kê</button>
+            </form>
+
+
+            <canvas id="myChart"></canvas>
+        </div>
     </div>
+
+    <!-- <tbody>
+        <?php $month = $_SESSION['listThangThongKe']; ?>
+        <?php foreach ($month as $item) : ?>
+        <tr>
+            <td>thang :<?php echo $item['thang']; ?></td>
+            <td>nam : <?php echo $item['nam']; ?></td>
+            <td>tong tien :<?php echo $item['tongTien']; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody> -->
 </body>
+
+<!-- <script src="../../../assets/css/Home/Home.js"></script> -->
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    // Định nghĩa dữ liệu cho biểu đồ
+    var data = {
+        labels: [],
+        datasets: [{
+            label: 'Doanh thu',
+            data: [],
+            backgroundColor: 'rgba(75, 192, 192, 0.5)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        }]
+    };
+
+    <?php foreach ($month as $item) : ?>
+    var label = '<?php echo date("M-Y", strtotime($item['thang'].'-'.$item['nam'])); ?>';
+    data.labels.push(label);
+    data.datasets[0].data.push(<?php echo $item['tongTien']; ?>);
+    <?php endforeach; ?>
+
+    var options = {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    };
+
+    // Lấy canvas và vẽ biểu đồ cột
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: options
+    });
+});
+</script>
 
 </html>
