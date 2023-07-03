7d80abed301b4ceb150bc904f518f21ac45adcaf
Quan Bui <bhquaninit27@gmail.com>
2023-07-03 13:54:44 +0700

    show image detail Product Page Member

--- GhepCode/Action-Controler/get_inforPR.php
+++ GhepCode/Action-Controler/get_inforPR.php
@@ -8,7 +8,7 @@ $sql =  "select * from product where proID = ".$prID;
 $result = $conn->query($sql);
 if($result->num_rows>0){
     $row = $result->fetch_assoc();
-    $_SESSION["product"] = new Product($row["proID"],$row["proName"],$row["grID"],$row["price"],$row["quantity"],$row["size"],$row["color"],$row["description"],$row["active"],$row["img"]);
+    $_SESSION["product"] = new Product($row["proID"],$row["proName"],$row["grID"],$row["price"],$row["quantity"],$row["size"],$row["color"],$row["description"],$row["active"],$row["image"]);
     $_SESSION["feedback"] = getListFB($prID);
     header("Location: /Project_WebBanHang/GhepCode/Template-View/product_infor.php");
 }else{
